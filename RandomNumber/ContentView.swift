b059c98009a9261ac2f01ea0f3df2962148cb565
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-03-11 11:08:11 +0900

    じゃんけん

--- RandomNumber/ContentView.swift
+++ RandomNumber/ContentView.swift
@@ -7,10 +7,70 @@
 
 import SwiftUI
 
+let janken = ["なし", "グー", "チョキ", "パー"]
+
 struct ContentView: View {
+    @State var jankenNum = 0
+    @State var enemyNum = 0
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            HStack{
+                Image("Enemy")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200)
+                Text("このわたしに\n勝てるかな？")
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
+            Image(janken[enemyNum])
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120)
+                .scaleEffect(x: -1, y: -1)
+            Spacer()
+            Image(janken[jankenNum])
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120)
+            HStack{
+                JankenButton(num: $jankenNum, jankenID: 1, color: Color.red)
+                JankenButton(num: $jankenNum, jankenID: 2, color: Color.yellow)
+                JankenButton(num: $jankenNum, jankenID: 3, color: Color.blue)
+            }
+            Button(action: {
+                enemyNum = Int.random(in: 1..<4)
+            }, label: {
+                Text("しょうぶ！")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .frame(width: 300, height: 75)
+                    .background(jankenNum == 0 ? Color.gray:Color.green)
+                    .cornerRadius(10)
+            })
+            .disabled(jankenNum == 0)
             .padding()
+        }
+    }
+}
+
+struct JankenButton: View {
+    @Binding var num: Int
+    let jankenID: Int
+    let color: Color
+    var body: some View {
+        Button(action: {
+            num = jankenID
+        }, label: {
+            Text(janken[jankenID])
+                .foregroundColor(.white)
+                .font(.title)
+                .frame(width: 100, height: 100)
+                .background(color)
+                .cornerRadius(10)
+                
+        })
     }
 }
 
