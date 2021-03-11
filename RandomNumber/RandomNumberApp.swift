771c669b993f7ed50836debaa5dd56f81bbd356f
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-03-11 09:50:19 +0900

    Initial Commit

---
+++ RandomNumber/RandomNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberApp.swift
+//  RandomNumber
+//
+//  Created by 澤崎正佳 on 2021/03/11.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
